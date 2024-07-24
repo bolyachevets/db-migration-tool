0be11de495528ca418163ec4ee55d9ca9de7bb59
bolyachevets <andriy.bolyachevets@gov.bc.ca>
2024-07-24 16:36:53 -0700

    count number of files in archive

--- run.sh
+++ run.sh
@@ -15,9 +15,28 @@ load_oc_db() {
   else
       echo "failed to successfully download from backups"
   fi
+
+  # Count files before extraction
+  count_before=$(ls -1 | wc -l)
+
+  # Extract files
+  gunzip $db_file
+
+  # Count files after extraction
+  count_after=$(ls -1 | wc -l)
+
+  # Calculate the number of new files
+  new_files=$((count_after - count_before))
+  echo "Number of files extracted: $new_files"
+
+  if [ $new_files -eq 2 ]; then
+    db_file="backup.sql"
+  fi
+
   gsutil cp $db_file "gs://${DB_BUCKET}/${db}/"
   gcloud --quiet sql databases delete $DB_NAME --instance=$GCP_SQL_INSTANCE
   gcloud sql databases create $DB_NAME --instance=$GCP_SQL_INSTANCE
+
   gcloud --quiet sql import sql $GCP_SQL_INSTANCE "gs://${DB_BUCKET}/${db}/${db_file}" --database=$DB_NAME --user=$DB_USER
   gcloud sql operations list --instance=$GCP_SQL_INSTANCE --filter='NOT status:done' --format='value(name)' | xargs -r gcloud sql operations wait --timeout=unlimited
 }
