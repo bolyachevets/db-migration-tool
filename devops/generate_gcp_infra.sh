9e5cfff20ce65b053f57ca906541844b2f3760ce
bolyachevets <andriy.bolyachevets@gov.bc.ca>
2024-08-24 02:12:51 -0700

    infra scripts

--- devops/generate_gcp_infra.sh
+++ devops/generate_gcp_infra.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 # Variables
 TARGET_PROJECT_ID=""
 INSTANCE_NAME="auth-db"
@@ -5,10 +7,20 @@ ENV="dev"
 REGION="northamerica-northeast1"
 BUCKET_NAME="auth-db-dump"
 OC_NAMESPACE=""
+HOST_PROJECT_ID=""
 DUMP_FILE="2024-07-26/postgresql-prod-auth-db_2024-07-26_01-00-00.sql.gz"
+DUMP_FILE="final_backup.sql"
 POSTGRES_VERSION="POSTGRES_15"
+SHARED_VPC_NAME="bcr-vpc"
+
+
+# gcloud sql instances describe "${INSTANCE_NAME}-${ENV}" --project="${TARGET_PROJECT_ID}-${ENV}" > instance-config.json
+# gcloud run jobs describe db-migration-tool --project="gtksf3-dev" --format=json > job-config.json
+
+# enable Service Networking API
 
 gcloud config set project "${TARGET_PROJECT_ID}-${ENV}"
+
 
 gcloud sql instances create "${INSTANCE_NAME}-${ENV}" \
     --database-version=$POSTGRES_VERSION \
@@ -25,10 +37,19 @@ gcloud sql instances create "${INSTANCE_NAME}-${ENV}" \
     --project="${TARGET_PROJECT_ID}-${ENV}" \
     --maintenance-window-day=MON \
     --maintenance-window-hour=4 \
-    --backup-start-time=08:00
+    --backup-start-time=08:00 \
+    --network="projects/${HOST_PROJECT_ID}-${ENV}/global/networks/${SHARED_VPC_NAME}-${ENV}"
+
+gcloud sql instances patch "${INSTANCE_NAME}-${ENV}" \
+    --no-assign-ip
+
+gcloud projects add-iam-policy-binding ${PROJECT_ID} \
+  --member="serviceAccount:sa-api@${TARGET_PROJECT_ID}-${ENV}.iam.gserviceaccount.com" \
+  --role="roles/cloudsql.client"
 
 gsutil mb -l $REGION "gs://${BUCKET_NAME}-${ENV}/"
 gsutil cp /dev/null -l $REGION "gs://${BUCKET_NAME}-${ENV}/${INSTANCE_NAME}"
+
 
 gcloud run jobs create db-migration-tool \
     --project="${TARGET_PROJECT_ID}-${ENV}" \
@@ -50,3 +71,8 @@ gcloud run jobs create db-migration-tool \
     --execution-environment=gen2 \
     --labels=cloud.googleapis.com/location=$REGION \
     --labels=run.googleapis.com/lastUpdatedTime=$(date -u +'%Y-%m-%dT%H:%M:%SZ')
+
+
+gcloud sql export sql auth-db-prod gs://auth-db-dump-prod/dump.sql.gz --database=auth-db
+gcloud sql databases create auth-db --instance=auth-db-tools
+gcloud --quiet sql import sql auth-db-tools gs://auth-db-dump-prod/dump.sql.gz --database=auth-db --user=postgres
